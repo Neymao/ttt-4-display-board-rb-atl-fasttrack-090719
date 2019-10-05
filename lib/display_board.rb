26f6718ccd47e0cccc034861889f2a5d575b76aa
Ferney Mauricio Arenas <neypaisa@hotmail.com>
2019-10-05 13:11:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,11 @@
 
+# *** Edit the line below ***
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
+      # Don't touch the following line.
+      # Don't touch the following lines.
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
 
 def display_board(board)
   puts" #{board[0]} | #{board[1]} | #{board[2]}  "
